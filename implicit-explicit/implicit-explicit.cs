d4138183642cadb721fdcb1c5dd125cc8ef1ba19
successiveprince <prince.kumar1@successive.tech>
2023-10-27 11:14:23 +0530

    Implicit Explicit Conversion

--- implicit-explicit/implicit-explicit.cs
+++ implicit-explicit/implicit-explicit.cs
@@ -10,7 +10,40 @@ namespace implicit_explicit
     {
         static void Main(string[] args)
         {
+            //Implicit Conversion
 
+            int a = 10;
+            float b = a;
+            double c = b;
+            decimal d = (decimal) b;
+
+            //Explicit Conversion
+
+            //--double to int
+            double age = 22.2;
+            int correctAge = Convert.ToInt32(age);
+            Console.WriteLine(correctAge);
+
+            //--float to int
+            float salary = 1232.32f;
+            int correctSalary = Convert.ToInt32(salary);
+
+            //--string to int
+            string weight = "23";
+            int correctWeight;
+            if (int.TryParse(weight, out correctWeight))
+            {
+                correctWeight = Convert.ToInt32(weight);
+                Console.WriteLine(correctWeight);
+            }
+
+            //--int to string
+            int height = 76;
+            string correctHeight = height.ToString();
+            Console.WriteLine(correctHeight);
+
+
+            Console.ReadKey();
         }
     }
 }
