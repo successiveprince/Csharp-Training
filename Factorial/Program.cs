f0d94e31ddae4f7c4226eb97ba78b1078e7bf4fe
successiveprince <prince.kumar1@successive.tech>
2023-11-09 15:42:35 +0530

    Factorial using Recursion

---
+++ Factorial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static int FindFactorial(int input)
+        {
+            if (input == 0 || input == 1)
+                return 1;
+            int fac;
+            fac = input * FindFactorial(input - 1);
+            return fac;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial Program using Recursion\n");
+            Console.Write("Enter the number: ");
+            string s =Console.ReadLine();
+            int input;
+            bool res = int.TryParse(s, out input);
+            if (res && input > 0)
+            {
+                Console.WriteLine("Factorial of {0} is {1}", input, FindFactorial(input));
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input!!!");
+            }
+            Console.ReadKey();
+             
+        }
+    }
+}
