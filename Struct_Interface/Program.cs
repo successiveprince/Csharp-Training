00d56e7b89a9866365ef3bd9400dc1d8ef62de31
successiveprince <prince.kumar1@successive.tech>
2023-11-15 12:24:31 +0530

    Use of interface inside the structure.

---
+++ Struct_Interface/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Interface
+{
+    interface Interface1
+    {
+        void Display();
+    }
+    interface Interface2
+    {
+        void Show();
+    }
+    struct MyStructure : Interface1 , Interface2
+    {
+        void Interface1.Display()
+        {
+            Console.WriteLine("This is Interface 1");
+        }
+        void Interface2.Show()
+        {
+            Console.WriteLine("This is Interface 2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interface1 A1;
+            Interface2 A2;
+
+            A1 = new MyStructure();
+            A2 = new MyStructure();
+
+            A1.Display();
+            A2.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
