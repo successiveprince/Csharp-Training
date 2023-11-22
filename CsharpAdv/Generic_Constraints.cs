7c3cd21267f44ada6b7610563297efdf20e2f18e
successiveprince <prince.kumar1@successive.tech>
2023-11-22 15:01:55 +0530

    Generics and Generics Constraints

---
+++ CsharpAdv/Generic_Constraints.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAdv_Day2
+{
+    class Generic_Constraints
+    {
+        public string name { get; set; }
+        public string location { get; set; }
+    }
+    public class GenericClass<T> where T : class        //For class
+    {
+        public void MyMethod(T input)
+        {
+            Console.WriteLine($"Hello {input}");
+        }
+    }
+    public class GenericStruct<T> where T : struct      //For structure
+    {
+        public T age;
+        public void MyMethod()
+        {
+            Console.WriteLine($"My age is {age}");
+        }
+    }
+    public class GenericNew<T> where T : new()          //For new()
+    {
+        public T message;
+        public void MyNewMethod(T para1 , T para2)
+        {
+            Console.WriteLine($"Message : {message}");
+            Console.WriteLine($"Position : {para1}");
+            Console.WriteLine($"Company : {para2}");
+        }
+    }
+    public class NewClass
+    {
+        public string position { get; set; }
+        public string company { get; set; }
+    }
+    public class GenericBaseClass<T> where T : BaseClass            //For Base Class
+    {
+        public void MyBaseMethod(T para)
+        {
+            Console.WriteLine($"Info : {para}");
+        }
+    }
+    public class BaseClass
+    {
+        public void CallBaseClass()
+        {
+            Console.WriteLine("Base Class is Called!!!");
+        }
+    }
+    public class DerivedClass : BaseClass
+    {
+        public string info { get; set; }
+    }
+    public class GenericInterface<T> where T : MyInterface          //For Interface
+    {
+        public void InterfaceMethod(T para)
+        {
+            Console.WriteLine($"Parameter : {para}");
+        }
+    }
+    public interface MyInterface
+    {
+        void InterfaceMethod();
+    }
+    public class MyInterfaceClass : MyInterface
+    {
+        public string name { get; set; }
+        public void InterfaceMethod()
+        {
+            Console.WriteLine("Interface Called!!!");
+        }
+    }
+}
--- CsharpAdv/Program.cs
+++ CsharpAdv/Program.cs
@@ -30,8 +30,48 @@ namespace CsharpAdv_Day2
             //Generics<string> G1 = new Generics<string>("maxx");
             //G1.TestFunction("World");
 
-            MyGenericClass MG = new MyGenericClass();
-            MG.GenericMethod<int, string>(10, "Hello");
+            //MyGenericClass MG = new MyGenericClass();
+            //MG.GenericMethod<int, string>(10, "Hello");
+
+
+
+            //-----------Generic Constraints---------------
+
+            //GenericClass<Generic_Constraints> myClass = new GenericClass<Generic_Constraints>();    // Generic Constraints for class
+            //Generic_Constraints G = new Generic_Constraints() { name = "Prince", location = "Noida" };
+            //myClass.MyMethod(G);
+
+
+
+            //GenericStruct<int> S = new GenericStruct<int>();                                       // Generic Constraints for structure
+            //S.age = 22;
+            //S.MyMethod();
+
+
+
+            //GenericNew<NewClass> newClass = new GenericNew<NewClass>();                           // Generic Constraints for new()
+            //NewClass n1 = new NewClass() { position = "Trainee", company = "Successive" };
+            //NewClass n2 = new NewClass() { position = "Associate", company = "Successive" };
+            //NewClass n3 = new NewClass() { position = "Senior Associate", company = "Successive" };
+
+            //newClass.message = n1;
+            //newClass.MyNewMethod(n2 , n3);
+
+
+
+            //GenericBaseClass<DerivedClass> GD = new GenericBaseClass<DerivedClass>();               // Generic Constraints for Base Class
+            //DerivedClass d = new DerivedClass() { info = "Prince Noida" };
+
+            //GD.MyBaseMethod(d);
+            //d.CallBaseClass();
+
+
+
+            GenericInterface<MyInterfaceClass> GI = new GenericInterface<MyInterfaceClass>();         // Generic Constraints for Interface
+            MyInterfaceClass I1 = new MyInterfaceClass() { name = "Prince" };
+            GI.InterfaceMethod(I1);
+
+
 
 
 
