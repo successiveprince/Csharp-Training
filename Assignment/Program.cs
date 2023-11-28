bb59180c9f5557cabf3b7f48afda37200f181cf6
successiveprince <prince.kumar1@successive.tech>
2023-11-28 10:25:57 +0530

    assignment

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,28 +6,74 @@ using System.Threading.Tasks;
 
 namespace Assignment
 {
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // Question 1
+    //        //TryCatch.Call();
+
+    //        //// Question 3
+    //        //Delegates.MyMethod();   
+
+    //        ////Question 4
+    //        //GenericDelegate.Method(); 
+
+    //        ////Question 5
+    //        //CheckOutput.CallMethod1();
+    //        //CheckOutput.CallMethod2();
+
+    //        //Question 6
+    //        //Async_Await.Method1();
+    //        //Async_Await.Method2();
+
+
+    //        Console.ReadKey();
+    //    }
+    //}
     class Program
     {
         static void Main(string[] args)
         {
-            // Question 1
-            TryCatch.Call();
-
-            // Question 3
-            Delegates.MyMethod();   
-
-            //Question 4
-            GenericDelegate.Method(); 
-
-            //Question 5
-            CheckOutput.CallMethod1();
-            CheckOutput.CallMethod2();
-
-            //Question 6
-            Async_Await.Method1();
-            Async_Await.Method2();
-
+            callMethod();
             Console.ReadKey();
+        }
+
+        public static async void callMethod()
+        {
+            Task<int> task = Method1();
+            Method2();
+            int count = await task;
+            Method3(count);
+        }
+
+        public static async Task<int> Method1()
+        {
+            int count = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    
+                    count += 1;
+                }
+                Task.Delay(100).Wait(1000);
+            });
+            return count;
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(" Method 2");
+            }
+        }
+
+        public static void Method3(int count)
+        {
+            Console.WriteLine("Total count is " + count);
         }
     }
 }
