a4f2d3722f63abe9221ef18ddf0eb57df4ab0a30
successiveprince <prince.kumar1@successive.tech>
2023-11-28 10:18:35 +0530

    LINQ Training

---
+++ LINQ_Training/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Prince", "Ajay", "Yash", "Govind", "Utkarsh", "Akshat" };
+
+            var name = names.Where(x => x.Contains('a'));
+
+            Console.WriteLine("Names with letter \'a\' :");
+            foreach (var i in name)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
