12f2a18aa68f4f42f0d9ece6fdd9b3fdffc81e2d
successiveprince <prince.kumar1@successive.tech>
2023-11-28 13:03:00 +0530

    LINQ Training

--- LINQ_Training/Program.cs
+++ LINQ_Training/Program.cs
@@ -6,20 +6,44 @@ using System.Threading.Tasks;
 
 namespace LINQ_Training
 {
+   
     class Program
     {
         static void Main(string[] args)
         {
             string[] names = { "Prince", "Ajay", "Yash", "Govind", "Utkarsh", "Akshat" };
 
-            var name = names.Where(x => x.Contains('a'));
+            var name = names.Where(x => x.Contains('a'));                      // LINQ Method Syntax
 
-            Console.WriteLine("LINQ");
+            Console.WriteLine("--------------LINQ---------------\n");
+
+            Console.WriteLine("LINQ Method Syntax");
+            
             Console.WriteLine("Names with letter \'a\' :");
             foreach (var i in name)
             {
                 Console.WriteLine(i);
             }
+            
+            Console.WriteLine("\nLINQ Query Syntax");
+            var ans = from n in names where n.Contains('a') select n;          // LINQ Query Syntax
+            foreach (var i in name)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] marks = { 23, 12, 43, 32, 23 };                              // Direct use of Aggregate Functions
+            Console.WriteLine("\nMax marks : " + marks.Max());
+            Console.WriteLine("Min marks : " + marks.Min());
+            Console.WriteLine("Average marks : " + marks.Average());
+            Console.WriteLine("Sum of marks : " + marks.Sum());
+            Console.WriteLine("Count : " + marks.Count());
+
+
+
+            MyClass.Method();                                               // Aggregate Functions in LINQ
+
+
             Console.ReadKey();
         }
     }
---
+++ LINQ_Training/Student.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Training
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+    public class MyClass
+    {
+        public static void Method()
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                new Student() {StudentID = 1 , StudentName = "Prince" , Age = 22},
+                new Student() {StudentID = 2 , StudentName = "Akshat" , Age = 20},
+                new Student() {StudentID = 3 , StudentName = "Ajay" , Age = 23},
+                new Student() {StudentID = 4 , StudentName = "Utkarsh" , Age = 21},
+                new Student() {StudentID = 5 , StudentName = "Yash" , Age = 25},
+                new Student() {StudentID = 6 , StudentName = "Govind" , Age = 19},
+                new Student() {StudentID = 7 , StudentName = "Prince" , Age = 18}
+            };
+
+            var oldest = studentList.Max(s => s.Age);                               // MAX
+            Console.WriteLine("\nMax age : " + oldest);
+
+            var youngest = studentList.Min(s => s.Age);                             // MIN
+            Console.WriteLine("\nMin age : " + youngest);
+
+            var totalStudent = studentList.Count(s => s.Age >=20);                  // COUNT
+            Console.WriteLine("\nTotal age >= 20 : " + totalStudent);
+
+            var sum = studentList.Sum(s => s.Age);                                  // SUM
+            Console.WriteLine("\nSum of age : " + sum);
+
+            var avg = studentList.Average(s => s.Age);                              // Average
+            Console.WriteLine("\nAverage of age : " + avg);
+
+            var sortAsc = studentList.OrderBy(s => s.StudentName);                  // OrderBy
+            var sortDesc = studentList.OrderByDescending(s => s.StudentName);       // OrderByDescending
+
+            Console.WriteLine("\nName in Ascending order : ");
+            foreach(var a in sortAsc)
+            {
+                Console.WriteLine(a.StudentName);
+            }
+            Console.WriteLine("\nName in Descending order : ");
+            foreach (var d in sortDesc)
+            {
+                Console.WriteLine(d.StudentName);
+            }
+
+            var secondLevelSortAsc = studentList.OrderBy(s => s.StudentName).ThenBy(s => s.Age);                       // ThenBy
+            var secondLevelSortDesc = studentList.OrderByDescending(s => s.StudentName).ThenByDescending(s => s.Age);  // ThenByDescending
+
+            Console.WriteLine("\nSecond Level of Sorting using ThenBy in Age : ");
+            foreach (var a in secondLevelSortAsc)
+            {
+                Console.WriteLine("{0} : {1} " , a.StudentName , a.Age);
+            }
+            Console.WriteLine("\nSecond Level of Sorting using ThenByDescending in Age : ");
+            foreach (var d in secondLevelSortDesc)
+            {
+                Console.WriteLine("{0} : {1} ", d.StudentName, d.Age);
+            }
+
+            var skip = studentList.Skip(2);                                             // Skip
+            var take = studentList.Take(3);                                             // Take
+            var skipWhile = studentList.SkipWhile(s => s.Age >= 20);                    // SkipWhile
+            var takeWhile = studentList.TakeWhile(s => s.Age >= 20);                    // TakeWhile
+
+            Console.WriteLine("\nSkip 2 elements : ");
+            foreach (var i in skip)
+            {
+                Console.WriteLine(i.StudentName);
+            }
+            Console.WriteLine("\nTake 3 elements : ");
+            foreach (var i in take)
+            {
+                Console.WriteLine(i.StudentName);
+            }
+            Console.WriteLine("\nSkipWhile Age >= 20 : ");
+            foreach (var i in skipWhile)
+            {
+                Console.WriteLine(i.StudentName);
+            }
+            Console.WriteLine("\nTakeWhile Age >= 20 ");
+            foreach (var i in takeWhile)
+            {
+                Console.WriteLine(i.StudentName);
+            }
+
+            List<int> marks = new List<int>() { 7, 10, 21, 30, 45, 50, 87 };
+            List<string> number = new List<string>() { null, "Two", "Three", "Four", "Five" };
+
+            Console.WriteLine("\n1st Element in marks : {0}", marks.First(s=> s % 2 == 0));
+            Console.WriteLine("\n1st Element in marks : {0}", number.First());
+            Console.WriteLine("\n1st Element in marks : {0}", number.FirstOrDefault();
+        }
+    }
+}
